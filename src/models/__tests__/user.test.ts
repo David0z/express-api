4b40280b0592f7011f7469dee53427291f80b36d
David0z <dawidczesak98@gmail.com>
2025-01-26 18:08:29 +0100

    added user model tests

--- src/models/__tests__/user.test.ts
+++ src/models/__tests__/user.test.ts
@@ -6,11 +6,11 @@ const userModel = new UserModel();
 
 describe('User model', () => {
   describe('Test user model methods', () => {
-    const user: User = {
+    let user: User = {
       email: 'testuser@test.com',
       firstName: 'TestFirstName',
       lastName: 'TestLastName',
-      role: 'user',
+      role: 'admin',
     };
 
     beforeAll(async () => {
@@ -35,6 +35,39 @@ describe('User model', () => {
       const createdUser = await userModel.create(userToCreate);
 
       expect(createdUser).toEqual({ id: createdUser.id, ...userToCreate });
+    });
+
+    it('fetches all users', async () => {
+      const users = await userModel.collection();
+      expect(users).toHaveLength(2);
+    });
+
+    it('fetches all users with query', async () => {
+      const users = await userModel.collection('admin');
+      expect(users).toHaveLength(1);
+    });
+
+    it('fetches one user by id', async () => {
+      const id = user.id as string;
+      const fetchedUser = await userModel.get(id);
+      expect(fetchedUser).toEqual(user);
+    });
+
+    it('updates the user', async () => {
+      const id = user.id as string;
+      const userObject = {
+        firstName: 'New Name',
+        lastName: 'New Last Name',
+      };
+      const fetchedUser = await userModel.update(userObject, id);
+      user = { ...user, ...userObject };
+      expect(fetchedUser).toEqual(user);
+    });
+
+    it('deletes the user', async () => {
+      const id = user.id as string;
+      const deletedUser = await userModel.remove(id);
+      expect(deletedUser).toEqual(user);
     });
   });
 });
